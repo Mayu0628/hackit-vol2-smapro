7b983e94ceffc09575a1c16f92b49588900767ec
nagisa <c1318728@st.kanazawa-it.ac.jp>
2024-03-08 00:41:03 +0900

    トップ画面からコース選択への画面遷移

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+'use client'
+import { useRouter } from "next/navigation"
+
 export default function Home() {
-  return <main>top</main>;
+
+  const router = useRouter();
+
+  return (
+    <>
+      <h1>top</h1>
+      <div>
+        <button onClick={() => router.push('/course-selection')}>
+          コース選択
+        </button>      
+        <button>遊び方</button>
+      </div>
+    </>
+  )
 }
