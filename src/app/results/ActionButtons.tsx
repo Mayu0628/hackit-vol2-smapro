a4d51ea2ebb76d090a94f0159f91261650a964c7
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2024-03-14 15:40:09 +0900

    テスト

---
+++ src/app/results/ActionButtons.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ActionButtons = () => {
+  return (
+    <div>
+      <button>ホームに戻る</button>
+      <button>コース選択</button>
+      <button>もう一度</button>
+    </div>
+  )
+}
+
+export default ActionButtons
