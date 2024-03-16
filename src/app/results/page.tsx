78850afe2ac9ae3dc8a312220e5d4ea181d011fa
nagisa <c1318728@st.kanazawa-it.ac.jp>
2024-03-17 01:20:47 +0900

    解説のポップアップ

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,7 +1,41 @@
+'use client'
 import React from "react";
+import Popup from "./popup";
 
 const Result = () => {
-  return <div>Result</div>;
+
+  const corrects = [
+    {
+      lang: "React",
+      bool: "◎"
+    },{
+      lang: "Go",
+      bool: "✕"
+    }
+
+  ]
+
+  return (
+    <>
+      <h1>results</h1>
+
+      {corrects.map((lang) => {
+        return(
+          <div className="langs">
+            <div>{lang.bool}</div>
+            <div>{lang.lang}</div>
+            <Popup buttonLabel="解説"/>
+          
+            <style jsx>{`
+              .langs{
+                display: flex
+              }
+            `}</style>
+          </div>
+        )
+      })}
+    </>
+  );
 };
 
 export default Result
