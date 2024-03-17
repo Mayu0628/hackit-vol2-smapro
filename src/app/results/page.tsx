9ea4065e06ed7aa1cb24708fd58a68ebb73b873d
nagisa <c1318728@st.kanazawa-it.ac.jp>
2024-03-17 14:44:06 +0900

    ボタン追加

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from "react";
 import Popup from "./popup";
+import Link from "next/link";
 
 const Result = () => {
 
@@ -34,6 +35,10 @@ const Result = () => {
           </div>
         )
       })}
+
+      <Link href='/'><button>ホームに戻る</button></Link>
+      <Link href='/course-selection'><button>コース選択</button></Link>
+      <Link href='/game-start'><button>もう一度</button></Link>
     </>
   );
 };
