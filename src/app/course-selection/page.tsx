b8365fd47b214e64669583264b2640ed14fd6da4
nagisa <c1318728@st.kanazawa-it.ac.jp>
2024-03-18 13:53:14 +0900

    ゲーム画面css

--- src/app/course-selection/page.tsx
+++ src/app/course-selection/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import '../styles/course-selection.css'
+import '@/styles/course-selection.css'
 
 const CourseSelection = () => {
   return (
