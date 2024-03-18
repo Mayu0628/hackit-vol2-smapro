55aada2968c1fbe33177e0640973a012f9048119
iropazu <c1342318@st.kanazawa-it.ac.jp>
2024-03-18 13:27:14 +0900

    スコア機能の修正

--- src/app/game-start/components/Score/Score.tsx
+++ src/app/game-start/components/Score/Score.tsx
@@ -4,32 +4,32 @@ import React, { useState } from 'react'
 interface ScoreProps {
     currentTypingIndex?: number;
     currentQuestionIndex?: boolean[];
-    SelectCourse?:string[];
+    SelectCourse?: string;
   }
   
 
-const Score: React.FC<ScoreProps> = ({ currentTypingIndex=0 ,currentQuestionIndex=[false], SelectCourse=["normal"]}) => {
+const Score: React.FC<ScoreProps> = ({ currentTypingIndex=0 ,currentQuestionIndex=[false], SelectCourse="normal"}) => {
     const[Score,setScore] = useState<number>(0)
     // タイピングのスコア計算
     setScore(Score + currentTypingIndex * 100);
     // クイズのスコア計算
     try {
-        if(SelectCourse[0]){
+        if(SelectCourse = "easy"){
+            for(let i=0; i<currentQuestionIndex.length;i++){
+                if (currentQuestionIndex[i]){
+                    setScore(Score + 500);
+                }
+            }
+        }else if(SelectCourse = "normal"){
             for(let i=0; i<currentQuestionIndex.length;i++){
                 if (currentQuestionIndex[i]){
                     setScore(Score + 1000);
                 }
             }
-        }else if(SelectCourse[1]){
+        }else if(SelectCourse = "hard"){
             for(let i=0; i<currentQuestionIndex.length;i++){
                 if (currentQuestionIndex[i]){
                     setScore(Score + 1500);
-                }
-            }
-        }else if(SelectCourse[2]){
-            for(let i=0; i<currentQuestionIndex.length;i++){
-                if (currentQuestionIndex[i]){
-                    setScore(Score + 500);
                 }
             }
         }
