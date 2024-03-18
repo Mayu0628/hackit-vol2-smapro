7ce48f767d31f4116dd23150a57f53adba7adc88
NonokaM <c1201071@gmail.com>
2024-03-18 19:09:15 +0900

    add: image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
 'use client'
 import GameRules from './game-rules/gameRoules'
 import Link from 'next/link'
+import Image from 'next/image'
 import { useGameData } from './GameDataProvider'
+import logo from '@/styles/images/logo.png'
+import catImage from '@/styles/images/cat-image.png'
 
 export default function Home() {
   return (
     <>
-      <h1>top</h1>
       <div>
+      <Image
+            src={logo}
+            height={500}
+            alt='cat-image'
+            className='image'
+        ></Image>
         <Link href='/course-selection'>
           <button>コース選択</button>
         </Link>
         <GameRules buttonLabel='遊び方' />
+      </div>
+      <div>
+        <Image
+            src={catImage}
+            height={500}
+            alt='cat-image'
+            className='image'
+        ></Image>
       </div>
     </>
   )
