3778ad58cd41cda8918c45b99d602d3702409bcc
iropazu <c1342318@st.kanazawa-it.ac.jp>
2024-03-18 14:45:14 +0900

    Score機能の実装

---
+++ src/app/game-start/components/Score/Score.tsx
@@ -0,0 +1,40 @@
+"use client"
+import React, { useState } from 'react'
+
+// データを受け取る
+interface ScoreProps {
+    currentTyping?: number;
+    currentQuestion?: boolean[];
+    SelectCourse?: string;
+  }
+  
+//以下、スコア計算
+
+const Score: React.FC<ScoreProps> = ({ currentTyping=0 ,currentQuestion=[false], SelectCourse="normal"}) => {
+    let Scor = 0;
+// タイピングの計算
+    Scor = currentTyping * 100
+// クイズ回答の計算
+    if(SelectCourse === "easy"){
+        for(let i=0; i<currentQuestion.length;i++){
+            if (currentQuestion[i]){
+                Scor += 500;
+                }
+        }
+    }else if(SelectCourse === "normal"){
+        for(let i=0; i<currentQuestion.length;i++){
+            if (currentQuestion[i]){
+                Scor += 1000;
+            }
+            }
+    }else if(SelectCourse === "hard"){
+        for(let i=0; i<currentQuestion.length;i++){
+            if (currentQuestion[i]){
+                Scor += 1500;
+            }
+        }
+    }
+    return Scor
+}
+
+export default Score
